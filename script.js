c45d4470a839035f1e15b2db9c53b8cf234f9cf2
Ioana Enescu <ioana.enescu@student.unitbv.ro>
2018-03-26 12:34:22 +0300

    26.03.18 - top nav responsive - responsive for 600px - responsive for 375px

--- script.js
+++ script.js
@@ -13,3 +13,11 @@ function topFunction() {
     document.documentElement.scrollTop = 0;
 }
 
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
